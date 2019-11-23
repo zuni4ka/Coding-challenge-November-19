d8041d25e51c41490ab86c326e12f1ecd8607799
Maria Plieshka <zuni4ka@gmail.com>
2019-11-23 18:40:46 +0100

    Add basic styling: colors, row seperation, icons

--- src/App.js
+++ src/App.js
@@ -8,42 +8,41 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
-      <main className="main">
-        <h1>Device Simulator Dashboard</h1>
-        <button className="add-device-button" type="button">
-          Add Device
-        </button>
-        <div className="table" role="table">
-          <div className="table-row" role="rowgroup">
-            <div className="table-column-header" role="columnheader">Name</div>
-            <div className="table-column-header" role="columnheader">Device ID</div>
-            <div className="table-column-header" role="columnheader">HW Key</div>
-            <div className="table-column-header" role="columnheader">Charge</div>
-            <div className="table-column-header" role="columnheader">GPS</div>
-            <div className="table-column-header" role="columnheader">Mileage</div>
-            <div className="table-column-header"></div>
-          </div>
-          {data.map((device) => {
-            return (
-              <div className="table-row" role="rowgroup">
-                <div className="table-cell" role="cell">{device.name}</div>
-                <div className="table-cell" role="cell">{device.id}</div>
-                <div className="table-cell" role="cell">{device.hwKey}</div>
-                <div className="table-cell" role="cell">{`${device.charge}%`}</div>
-                <div className="table-cell" role="cell">{device.isGPSEnabled ? 'on' : 'off'}</div>
-                <div className="table-cell" role="cell">{device.mileage}</div>
-                <div className="table-cell" role="cell">
-                  <button class="edit-device-button" type="button">Edit</button>
-                  <button class="delete-device-button" type="button">Delete</button>
+      <main className="dashboard-container">
+          <button className="add-device-button" type="button">
+            ✚ Add Device
+          </button>
+          <h1 className="dashboard-name">Device Simulator Dashboard</h1>
+          <div className="table" role="table">
+            <div className="table-row header-row" role="rowgroup">
+              <div className="table-column-header" role="columnheader">Name</div>
+              <div className="table-column-header" role="columnheader">Device ID</div>
+              <div className="table-column-header" role="columnheader">HW Key</div>
+              <div className="table-column-header" role="columnheader">Charge</div>
+              <div className="table-column-header" role="columnheader">GPS</div>
+              <div className="table-column-header" role="columnheader">Mileage</div>
+              <div className="table-column-header"></div>
+            </div>
+            {data.map((device) => {
+              return (
+                <div className="table-row" role="rowgroup">
+                  <div className="table-cell" role="cell">{device.name}</div>
+                  <div className="table-cell" role="cell">{device.id}</div>
+                  <div className="table-cell" role="cell">{device.hwKey}</div>
+                  <div className="table-cell" role="cell">{`${device.charge}%`}</div>
+                  <div className="table-cell" role="cell">{device.isGPSEnabled ? 'On' : 'Off'}</div>
+                  <div className="table-cell" role="cell">{device.mileage}</div>
+                  <div className="table-cell" role="cell">
+                    <button className="device-actions edit-device-button" type="button"></button>
+                    <button className="device-actions delete-device-button" type="button"></button>
+                    <span className="button-text">Edit</span>
+                    <span className="button-text">Delete</span>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
       </main>
-
-
-
     </div>
   );
 }
