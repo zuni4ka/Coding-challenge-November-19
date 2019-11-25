897168737f95566cdae8386ad5adf65522a0af7f
Maria Plieshka <zuni4ka@gmail.com>
2019-11-25 17:01:23 +0100

    Deploy #1

---
+++ precache-manifest.f8818954ec403d4ed9e24b7adeb5d99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566a3921d0c9cdfc55c5a55806d69ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e559577417dcdd1254",
+    "url": "/static/css/main.41ae9087.chunk.css"
+  },
+  {
+    "revision": "f7a0ccffbea5babfd227",
+    "url": "/static/js/2.51aa31f7.chunk.js"
+  },
+  {
+    "revision": "f7e559577417dcdd1254",
+    "url": "/static/js/main.f243738b.chunk.js"
+  },
+  {
+    "revision": "1fad59293ccbb612172e",
+    "url": "/static/js/runtime-main.50f63c84.js"
+  },
+  {
+    "revision": "fe7aacf1112f8d0f922beae9d79d4dc0",
+    "url": "/static/media/delete.fe7aacf1.svg"
+  },
+  {
+    "revision": "8cbc23b0c0bb7f6da23d1e51165ea6e8",
+    "url": "/static/media/edit.8cbc23b0.svg"
+  }
+]);
